ddcb5e4518cea87aa6454542f66fdc3d45d1c89e
FelixGonzalez <felix.gonzalez3@upr.edu>
2016-11-16 21:35:27 -0500

    Testing login with get dummy data

--- phase1/assorum/www/js/controllers.js
+++ phase1/assorum/www/js/controllers.js
@@ -1,13 +1,13 @@
 angular.module('assorum.controllers', [])
 
 //login page controller
-.controller('loginCtrl', function($scope,$state) {
+.controller('loginCtrl', function($scope, user,$state) {
     $scope.login = {};
 
     //Submit function
     $scope.saveLogin = function() {
         //email or username/Password where filled
-        if($scope.login.EorU && $scope.login.password){
+        if(user.getUser($scope.login.EorU,$scope.login.password)){
             //change of state
             $state.go('tab.home');
         }else{
