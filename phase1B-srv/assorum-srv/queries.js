8971538f584af1e6dda960714c426ada9580171b
Emmanuel Ramos <emmanuel.ramos2@upr.edu>
2016-11-16 21:33:31 -0400

    update gitignore/ events queries to DB

---
+++ phase1B-srv/assorum-srv/queries.js
@@ -0,0 +1,99 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://emmanuelramos:emaema.@localhost:5432/assorum';
+var db = pgp(connectionString);
+
+// add query functions
+function getAllEvents(req, res, next) {
+  db.any('select * from events')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL events'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function getSingleEvent(req, res, next) {
+  var eventID = parseInt(req.params.id);
+  db.one('select * from events where id = $1', eventID)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ONE event'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function createEvent(req, res, next) {
+  req.body.age = parseInt(req.body.age);
+  db.none('insert into events(name, description, location, date, association, img)' +
+      'values(${name}, ${description}, ${location}, ${date}, ${association}, ${img})',
+    req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted one event'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function updateEvent(req, res, next) {
+  db.none('update events set name=$1, description=$2, location=$3, date=$4, association=$5, img=$6 where id=$7',
+    [req.body.name, req.body.breed, parseInt(req.body.age),
+      req.body.sex, parseInt(req.params.id)])
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Updated event'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function removeEvent(req, res, next) {
+  var eventID = parseInt(req.params.id);
+  db.result('delete from events where id = $1', eventID)
+    .then(function (result) {
+      /* jshint ignore:start */
+      res.status(200)
+        .json({
+          status: 'success',
+          message: `Removed ${result.rowCount} event`
+        });
+      /* jshint ignore:end */
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+module.exports = {
+  getAllEvents: getAllEvents,
+  getSingleEvent: getSingleEvent,
+  createEvent: createEvent,
+  updateEvent: updateEvent,
+  removeEvent: removeEvent
+};
