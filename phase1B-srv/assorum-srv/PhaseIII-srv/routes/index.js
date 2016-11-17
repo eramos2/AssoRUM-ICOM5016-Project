215369850107929fe2f4a8a9aaee3dd2ce142982
Emmanuel Ramos <emmanuel.ramos2@upr.edu>
2016-11-16 23:53:16 -0400

    added communication between server and DB for testing

--- phase1B-srv/assorum-srv/PhaseIII-srv/routes/index.js
+++ phase1B-srv/assorum-srv/PhaseIII-srv/routes/index.js
@@ -9,6 +9,7 @@ router.get('/api/events/:id', db.getSingleEvent);
 router.post('/api/events', db.createEvent);
 router.put('/api/events/:id', db.updateEvent);
 router.delete('/api/events/:id', db.removeEvent);
-
+router.get('/api/users', db.getAllUsers);
+router.get('/api/users/:username', db.getSingleUser);
 
 module.exports = router;
