bd8e81cda79972345a941ae50c88a585b9039412
Emmanuel Ramos <emmanuel.ramos2@upr.edu>
2016-11-18 16:47:36 -0400

    added db connectionString
    Added db connectionString for connecting to db in heroku

--- phase1B-srv/assorum-srv/PhaseIII-srv/queries.js
+++ phase1B-srv/assorum-srv/PhaseIII-srv/queries.js
@@ -6,7 +6,8 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://emmanuelramos:emaema.@localhost:5432/assorum';
+//var connectionString = 'postgres://emmanuelramos:emaema.@localhost:5432/assorum';
+var connectionString = 'postgres://umvqzgtzegopge:Mk7KHzN4igK5H1Ub8IEAbTFugo@ec2-54-243-207-17.compute-1.amazonaws.com:5432/d2t0un16n28uoo';
 var db = pgp(connectionString);
 
 // add query functions
